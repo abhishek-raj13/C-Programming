2e78f435c6b4668cc4e43b067a86d051d86b38f3
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-09 10:16:40 +0530

    Add files via upload

---
+++ 02_variables_rules.c
@@ -0,0 +1,13 @@
+/* No special symbol other than (_)allowed
+No commas, blanks are allowed 
+First Character must be an alphabet or underscore*/
+/*Constant
+An entity whose values does not change is called as a constant
+A variable is an entity whose value can be changed */
+#include<stdio.h>
+int mian (){
+    int abhi;
+    int Abhi;
+    int abhi_good;
+    return 0 ;
+}
