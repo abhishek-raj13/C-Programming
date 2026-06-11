886623f911a4b0a8612f652d4bd1bed15e1fd831
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-11 10:32:35 +0530

    Add files via upload

---
+++ 07_escape_sequence.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+intmain(){
+    int a;
+    vhar ch = 'a';
+    ch = 'c';
+    ch = '\n'; // represents a new line 
+    printf("Hey I am good \\nice");
+                /*         \n
+                            \t etc
+                        
+                                */
+}
