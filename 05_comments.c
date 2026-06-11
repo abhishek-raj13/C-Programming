712408bd87cfdc512654178e6826ef4e5da8e038
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-11 10:31:56 +0530

    Add files via upload

---
+++ 05_comments.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    // Iam writting this just for fun
+    printf("Hello World");
+    //Return 0 is returning 0 to the OS
+    return 0; // This is return
+    // This is for Single line comment
+    /*this is for multi line comments*/
+}
