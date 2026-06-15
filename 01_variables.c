73c512a306ba72522c0366c1541bd3fbbd1cb5c5
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-15 17:36:29 +0530

    Add files via upload

--- 01_variables.c
+++ 01_variables.c
@@ -1,7 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(){
-    int a; // variable declaration
-    a=6; // variable initializtion
-    printf("The output of this program is %d",a);
+    int i =10;//Declere and initialize 'i' with10
+    int j =i;
+    int a =2, b=3,c=4,d=5;
+    //%d is called format specifier 
+    // %d is for int , %f is for float, %c is for char
+    printf(" The value of i is %d and value of j is %d\n",i,j);
+    printf(" The value of a is %d and the value of b is %d\n",a,b);
+    printf(" The value of c is %d and the value of d is %d\n",c,d);
     return 0;
 }
