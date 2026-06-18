b88dda181a0c2cacb84310b29f8d0dbd31994baf
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-18 20:38:30 +0530

    Add files via upload

---
+++ 01_conditionals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int age = 15;
+    if(age>10){
+        printf("We are inside if\n");
+        printf("Your age is greater than 10\n");
+    }
+    if(age%5==0){
+        // %5== 0 means remainder is 0 if we divide age by 5
+        printf("We are inside another if\n");
+        printf("Your age is divisible by 5\n");
+    }
+    return 0;
+}
