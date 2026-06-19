044a217eaab8dc4b70abac19d11fac633b9ec55f
Abhishek Raj Bharti <abhishekyadav130904@gmail.com>
2026-06-19 17:46:39 +0530

    Add files via upload

---
+++ 06_ternary.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    // condition? expression-if-true : expression-if-false
+    int a = 345;
+    int b = 32;
+    a>b?printf("a is greater"):printf("b is greater");
+}
