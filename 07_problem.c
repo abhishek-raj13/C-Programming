5ca353bf6cfe4fac545571efa09afca299ca8a6a
Abhishek Raj Bharti <abhishekydv139@gmail.com>
2026-08-06 22:25:55 +0530

    Add files via upload

---
+++ 07_problem.c
@@ -0,0 +1,20 @@
+/*7. Write a program to count the occurrence of a given character in a string. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char c = 'r';
+    int count = 0;
+    char str[] = "Abhi";
+    for (int i = 0; i < strlen(str); i++)
+    {
+       if(str[i] == c){
+        count++;
+       }
+    }
+
+    printf("%d", count);
+    
+    return 0;
+}
