2c0fc41c8dab98fbd20d717984d89fe077b8265d
Abhishek Raj Bharti <abhishekydv139@gmail.com>
2026-08-11 21:43:40 +0530

    Add files via upload

---
+++ 03_problem.c
@@ -0,0 +1,22 @@
+/*3. Twenty integers are to be stored in memory. What will you prefer- Array or 
+structure?*/
+
+#include <stdio.h>
+typedef struct emp{
+    int salary;
+    float score;
+} Employee;
+
+int main(){
+    Employee e1;
+    Employee* ptr = &e1;
+    (*ptr).salary = 56;
+    (*ptr).score = 45.3;
+
+    // ptr->salary = 56;
+    // ptr->score = 45.3;
+
+    printf("The value of salary is %d and the value of score is %.2f \n", ptr->salary, ptr->score);
+    
+    return 0;
+}
